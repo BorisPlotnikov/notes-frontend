d639b64ed4d00a47f5a386a693c4aace628868b8
BorisPlotnikov <boriswo@gmail.com>
2025-06-18 08:22:12 -0700

    parseError.js is added.

---
+++ src/utils/parseError.js
@@ -0,0 +1,47 @@
+// utils/parseError.js
+
+const parseError = (error = 'Unknown error', log = 'An operation failed.') => {
+    let userMessage = 'An unexpected error occurred';
+    let logMessage = log;
+
+    const checkStatus = (statusCode) => {
+        const statusMessages = {
+            400: 'Bad Request. Please check your input.',
+            401: 'Unauthorized. Please log in.',
+            404: 'Reasource not found.',
+            408: 'Request timed out. Please check your network connection and try again.',
+            500: 'Internal Server Error. Please try again later.',
+        };
+        return statusMessages[statusCode] || userMessage;
+    };
+
+    const checks = [
+        {
+            check: () => typeof error === 'string',
+            getUserMessage: () => error
+        },
+        {
+            check: () => error?.response?.status,
+            getUserMessage: () => checkStatus(error.response.status)
+        },
+        {
+            check: () => error?.message,
+            getUserMessage: () => error.message
+        },
+        {
+            check: () => error?.request,
+            getUserMessage: () => 'No response from the server. Check your network.'
+        },
+    ];
+
+    for (const { check, getUserMessage } of checks) {
+        if (check()) {
+            userMessage = getUserMessage();
+            break;
+        }
+    }
+
+    return { userMessage, logMessage, stack: error?.stack };
+};
+
+export default parseError;
