9cbe775588e13033a8f6a07ea6130741ba51ca9c
BorisPlotnikov <boriswo@gmail.com>
2025-06-21 21:53:53 -0700

    parseError is refactored.

--- src/utils/parseError.js
+++ src/utils/parseError.js
@@ -2,40 +2,23 @@
 
 import { ERROR_MESSAGES } from '../constants';
 
-const parseError = (error = ERROR_MESSAGES.DEFAULT.USER, logMessage = ERROR_MESSAGES.DEFAULT.LOG) => {
-    let userMessage = ERROR_MESSAGES.DEFAULT.USER;
-
-    const checkStatus = (statusCode) => {
-        return ERROR_MESSAGES.HTTP_STATUS[statusCode] || ERROR_MESSAGES.DEFAULT.USER;
-    };
-
-    const checks = [
-        {
-            check: () => typeof error === 'string',
-            getUserMessage: () => error
-        },
-        {
-            check: () => error?.response?.status,
-            getUserMessage: () => checkStatus(error.response.status)
-        },
-        {
-            check: () => error?.message,
-            getUserMessage: () => error.message
-        },
-        {
-            check: () => error?.request,
-            getUserMessage: () => ERROR_MESSAGES.NETWORK.NO_RESPONSE
-        },
-    ];
-
-    for (const { check, getUserMessage } of checks) {
-        if (check()) {
-            userMessage = getUserMessage();
-            break;
-        }
-    }
-
-    return { userMessage, logMessage, stack: error?.stack };
-};
+const parseError = (
+    error = ERROR_MESSAGES.DEFAULT.USER,
+    logMessage = ERROR_MESSAGES.DEFAULT.LOG
+  ) => {
+    const userMessage =
+      typeof error === 'string' ? error
+      : error?.response?.status ? ERROR_MESSAGES.HTTP_STATUS[error.response.status] || ERROR_MESSAGES.DEFAULT.USER
+      : error?.message ? error.message
+      : error?.request ? ERROR_MESSAGES.NETWORK.NO_RESPONSE
+      : ERROR_MESSAGES.DEFAULT.USER;
+  
+    const stack = parseError.stackTraceEnabled ? error?.stack : undefined;
+  
+    return { userMessage, logMessage, stack };
+  };
+  
+  parseError.stackTraceEnabled = true;
+  
 
 export default parseError;
