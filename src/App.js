b0d8a386f764598654b61f26658f7ddfcbaf994e
BorisPlotnikov <boriswo@gmail.com>
2025-07-31 01:33:55 -0700

    App.js is splitted into NotesApp.js and App.js.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 // /src/App.js
 
-import React from 'react';                     
+import React from 'react';
+import NotesApp from './features/notes/NotesApp';                     
 import './css/App.css';
-import AppLoader from './components/AppLoader';
-import NoteList from './features/notes/components/NoteList';
-import NoteForm from './features/notes/components/NoteForm';
-import { useNotes } from './features/notes/context/NotesContext';
 
 const App = () => {
-    const { isInitialized } = useNotes();
 
     return (
             <main className='app'>
-                <h1>Notes</h1>
-                <NoteForm />
-                {!isInitialized ? <AppLoader /> : <NoteList />}
+                <NotesApp />
             </main>
     );
 };
--- src/features/notes/NotesApp.js
+++ src/features/notes/NotesApp.js
@@ -1,1 +1,21 @@
+// /src/features/notes/NotesApp.js
+
 import React from 'react';
+import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
+import AppLoader from '../../components/AppLoader';
+import { useNotes } from './context/NotesContext';
+
+const NotesApp = () => {
+    const { isInitialized } = useNotes();
+
+    return (
+        <>
+            <h1>Notes</h1>
+            <NoteForm />
+            {!isInitialized ? <AppLoader /> : <NoteList />}
+        </>
+    );
+};
+
+export default NotesApp;
